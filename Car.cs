cbb6971768083850b33abd32f9aa37b1fd0f1348
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-02-07 20:41:07 -0500

    Rough Draft

---
+++ Car.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class Car
+    {
+
+        public Car()
+        {
+            Random random = new Random();
+
+            Pressure = random.Next(14, 26);
+            OilLevel = random.Next(14, 30);
+        }
+
+
+        public int Pressure { get; set; }
+        public int OilLevel { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        //public int DaysInShop { get; set; }
+
+
+
+
+        public string SetModel(string model)
+        {
+            Model = model;
+            return Model;
+        }
+
+        public string SetMake(string make)
+        {
+            Make = make;
+            return Make;
+        }
+
+
+        public int GetPressure()
+        {
+            return Pressure;
+        }
+
+        public int GetOil()
+        {
+            return OilLevel;
+        }
+
+        public string GetColor()
+        {
+            return Color;
+        }
+
+        public void ChangeOil()
+        {
+            OilLevel += 25;
+            if (OilLevel > 100)
+            {
+                OilLevel = 100;
+            }
+        }
+
+        public void DumpOil()
+        {
+            OilLevel = 0;
+        }
+
+        public void DumpPressure()
+        {
+            Pressure = 0;
+        }
+
+
+
+        public void AddPressure()
+        {
+            Pressure += 6;
+        }
+
+        public string SetColor(string color)
+        {
+            Color = color;
+            return Color;
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,108 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
-            
+            Car car = new Car();
+            Console.WriteLine("Welcome to the car repair shop.\nPlease enter some info about your car and we can get started.");
+            Console.WriteLine("Enter the make of the car.");
+            car.SetMake(Console.ReadLine());
+            Console.WriteLine("Enter the model of the car.");
+            car.SetModel(Console.ReadLine());
+            Console.WriteLine("Enter the color of your car.");
+            car.SetColor(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Your {car.Color} {car.Make} {car.Model} is now in the shop");
+            Console.WriteLine("Press enter to view service options");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Service Menu
+
+            bool ServiceShop = true;
+
+            do
+            {
+
+                Console.WriteLine("1. See the overall status of your car.");
+                Console.WriteLine("2. Paint a different color");
+                Console.WriteLine("3. Add air pressure to tires");
+                Console.WriteLine("4. Let out all tire pressure.");
+                Console.WriteLine("5. Change oil.");
+                Console.WriteLine("6. Dump Oil.");
+                Console.WriteLine("7. Exit the repair shop.");
+
+                string userChoice = Console.ReadLine().ToUpper();
+                Console.Clear();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine($"{car.Color} {car.Make} {car.Model}\n" +
+                            $"Oil Life:{car.GetOil()}\nTire Pressure:{car.GetPressure()}");
+                        break;
+
+                    case "2":
+                        Console.WriteLine($"What color would you like to paint your car?");
+                        car.SetColor(Console.ReadLine());
+                        Console.WriteLine($"Your car is now {car.Color}");
+                        break;
+
+                    case "3":
+                        car.AddPressure();
+                        if (car.GetPressure() > 50)
+                        {
+                            Console.WriteLine("WHoa there. Lets not over inflate them. Check your manual");
+                        }
+                        else
+                        Console.WriteLine("Air has been added to your tires");
+                        Console.WriteLine($"Your tire pressure is now  {car.GetPressure()}");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("All the pressure in your tires has been let out.");
+                        car.DumpPressure();
+                        break;
+
+                    case "5":
+                        car.ChangeOil();
+                        if (car.GetOil() > 70)
+                        {
+                            Console.WriteLine("Make sure you don't add to much.");
+                        }
+                        else
+                        Console.WriteLine("Your oil has been changed.");
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Your oil has been completely purged.");
+                        car.DumpOil();
+                        break;
+
+                    case "7":
+                        ServiceShop = false;
+                        Console.WriteLine("See you next time!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid selection.");
+                        break;
+
+                }
+                Console.WriteLine("Return to main menu");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+            while (ServiceShop);
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
